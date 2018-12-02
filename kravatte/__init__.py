21a6da899fe2707ff6128a89f83e3aeecf2ea68a
Calvin McCoy <calvin.mccoy@gmail.com>
2018-12-02 10:01:43 -0800

    Bump version: 1.1.0 → 1.2.0

--- kravatte/__init__.py
+++ kravatte/__init__.py
@@ -4,4 +4,4 @@
 from .kravatte import Kravatte, KravatteSAE, KravatteSANE, KravatteSANSE, KravatteWBC, KravatteWBC_AE, siv_wrap, siv_unwrap, mac, KravatteTagOutput, KravatteValidatedOutput, KravatteOracle
 __author__ = """Michael Calvin McCoy"""
 __email__ = 'calvin.mccoy@protonmail.com'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
